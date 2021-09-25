70d5fb2558dc3280af75a9cebcee2cbf298cb729
Agathe Sadeghi <55772710+Agatheee@users.noreply.github.com>
2021-09-25 12:36:56 -0400

    Update flask_test.py

--- flask_test.py
+++ flask_test.py
@@ -1,7 +1,7 @@
 from flask import Flask
 app = Flask(__name__) #"__main__"
 
-@app.route('\hello' , methods=['GET'])
+@app.route('/hello' , methods=['GET'])
 def hello_world():
   return "Hello World! This is Agathe :)"
 
